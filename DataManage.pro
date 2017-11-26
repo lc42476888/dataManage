2c3aef00495162438ece54f2481d06f8645b352f
lc42476888 <33975444+lc42476888@users.noreply.github.com>
2017-11-27 00:26:06 +0800

    ops,delete some useless file
    just find out that it is difficult to make a apps perfect.

--- DataManage.pro
+++ DataManage.pro
@@ -14,13 +14,16 @@ TEMPLATE = app
 
 SOURCES += main.cpp\
         widget.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    outfiledialog.cpp
 
 HEADERS  += widget.h \
-    mainwindow.h
+    mainwindow.h \
+    outfiledialog.h
 
 FORMS    += widget.ui \
-    mainwindow.ui
+    mainwindow.ui \
+    outfiledialog.ui
 
 RESOURCES += \
     datamanageresource.qrc
--- widget.cpp
+++ widget.cpp
@@ -10,6 +10,11 @@ Widget::Widget(QWidget *parent) :
     connect(ui->cancelPushButton,SIGNAL(clicked(bool)),this,SLOT(cancelConnection()));
     QList<QString> availibleDatabase = QSqlDatabase::drivers();
     ui->databaseDriversComboBox->addItems(availibleDatabase);
+    ui->ipLabel->setVisible(false);
+    ui->ipLineEdit->setVisible(false);
+    ui->portLabel->setVisible(false);
+    ui->portLineEdit->setVisible(false);
+    connect(ui->netConnectPushButton,SIGNAL(toggled(bool)),this,SLOT(setConnectName()));
 //    qssFile = new QFile(":/qss/my.qss",this);
 //    qssFile->open(QFile::ReadOnly);
 //    QString style=tr(qssFile->readAll());
@@ -25,25 +30,81 @@ Widget::~Widget()
 void Widget::creatConnection()
 {
 
-    QString users,pw,dbDriver;
+    QString users,pw,dbDriver,port;
     users=ui->userLineEdit->text();
     pw=ui->pwLineEdit->text();
     dbDriver=ui->databaseDriversComboBox->currentText();
     QSqlDatabase db =QSqlDatabase::addDatabase(dbDriver);
-    db.setHostName("localhost");
-    db.setUserName("root");
-    db.setPassword(pw);
-    if(!db.open())
+
+    if(ui->netConnectPushButton->isChecked())
     {
-        QMessageBox::warning(this,tr("登录失败"),
-                             tr("密码错误或该数据库不存在"),QMessageBox::Ok);
+        QRegExp reg("\\b((?:(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.)"
+                    "{3}(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d))\\b");
+        if(!reg.exactMatch(ui->ipLineEdit->text()))
+        {
+            QMessageBox::warning(this,"jinggao","请输入正确的ip地址",QMessageBox::Yes);
+            qDebug()<<reg.matchedLength();
+        }
+        else
+        {
+             db.setPort(ui->portLineEdit->text().toInt());
+             db.setHostName(ui->ipLineEdit->text());
+             db.setUserName(ui->userLineEdit->text());
+             db.setPassword(pw);
+             if(!db.open())
+             {
+                 QMessageBox::warning(this,tr("登录失败"),
+                                      tr("密码错误或该数据库不存在"),QMessageBox::Ok);
+             }
+             else
+             {
+                 MainWindow *w2= new MainWindow();
+                 w2->show();
+                 close();
+             }
+        }
     }
     else
     {
-        MainWindow *w2= new MainWindow();
-        w2->show();
-        close();
+        db.setHostName("localhost");
+        db.setUserName(ui->userLineEdit->text());
+        db.setPassword(pw);
+        if(!db.open())
+        {
+            QMessageBox::warning(this,tr("登录失败"),
+                                 tr("密码错误或无法连接至数据库"),QMessageBox::Ok);
+        }
+        else
+        {
+            MainWindow *w2= new MainWindow();
+            w2->show();
+            close();
+        }
     }
+
+
+}
+
+void Widget::setConnectName()
+{
+    if (ui->netConnectPushButton->isChecked())
+    {
+        ui->ipLabel->setVisible(true);
+        ui->ipLineEdit->setVisible(true);
+        ui->portLabel->setVisible(true);
+        ui->portLineEdit->setVisible(true);
+        ui->netConnectPushButton->setText("切换至本地登录");
+    }
+    else
+    {
+        ui->ipLabel->setVisible(false);
+        ui->ipLineEdit->setVisible(false);
+        ui->portLabel->setVisible(false);
+        ui->portLineEdit->setVisible(false);
+        ui->netConnectPushButton->setText("切换至远程连接");
+
+    }
+
 
 }
 
@@ -52,3 +113,4 @@ void Widget::cancelConnection()
 
     close();
 }
+
